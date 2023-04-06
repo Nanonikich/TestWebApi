6e2e32c5c9524adfcffbe03f2818bb25c2a4acca
Nanonikich <nanonikich@gmail.com>
2023-04-06 11:20:54 +0300

    Added test web api

---
+++ readme.md
@@ -0,0 +1,11 @@
+# Тестовый проект Docker
+
+Используются технологии:
+- ASP.NET Core;
+- React;
+- NGINX;
+- Docker Compose.
+
+Обращение клиента к API происходит по нажатию кнопки:
+
+![result](screenshots/result.jpg "Результат запроса")
---
+++ src/service.api.prj/Controllers/UsersController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Service.Api.Controllers;
+
+[Route("api/users")]
+[ApiController]
+public class UsersController : ControllerBase
+{
+	[HttpGet]
+	public IActionResult GetUsers()
+	{
+		var users = new[]
+		{
+			new { Name = "Nikita" },
+		};
+
+		return Ok(users);
+	}
+}
